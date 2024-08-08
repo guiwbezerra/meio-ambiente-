1c8ce9ea3db0c45845011e9fd1989d030b7dddfd
guiwbezerra <130068277+guiwbezerra@users.noreply.github.com>
2024-08-08 09:01:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,97 +1,29 @@
+import {aleatorio} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-// Definição da história e perguntas sobre meio ambiente
-const perguntas = [
-    {
-        enunciado: "Você cresceu em uma cidade rodeada por natureza e sempre apreciou suas belezas naturais. Como você se sente sobre a atual situação ambiental?",
-        alternativas: [
-            {
-                texto: "Estou muito preocupado com as mudanças climáticas e a perda de biodiversidade.",
-                afirmacao: "Preocupação com o futuro do planeta."
-            },
-            {
-                texto: "Acredito que a natureza é resiliente e que os problemas ambientais se resolverão sozinhos.",
-                afirmacao: "Confiança na capacidade da natureza de se recuperar."
-            }
-        ]
-    },
-    {
-        enunciado: "Você está planejando suas férias. Como você escolhe o destino?",
-        alternativas: [
-            {
-                texto: "Prefiro destinos próximos à natureza, como parques nacionais e praias limpas.",
-                afirmacao: "Preferência por destinos ecológicos e sustentáveis."
-            },
-            {
-                texto: "Escolho destinos urbanos movimentados, pois são mais excitantes e oferecem mais opções de entretenimento.",
-                afirmacao: "Preferência por destinos urbanos e movimentados."
-            }
-        ]
-    },
-    {
-        enunciado: "Ao fazer compras, como você escolhe produtos?",
-        alternativas: [
-            {
-                texto: "Prefiro produtos orgânicos e locais para apoiar a agricultura sustentável.",
-                afirmacao: "Apoio ao consumo consciente e sustentável."
-            },
-            {
-                texto: "Escolho produtos com base no preço e na conveniência, sem considerar muito o impacto ambiental.",
-                afirmacao: "Preferência por conveniência e preço."
-            }
-        ]
-    },
-    {
-        enunciado: "Você é convidado para um protesto ambiental. Qual é a sua reação?",
-        alternativas: [
-            {
-                texto: "Participo ativamente para lutar por políticas ambientais mais rigorosas.",
-                afirmacao: "Engajamento em causas ambientais e ativismo."
-            },
-            {
-                texto: "Prefiro não me envolver em protestos e busco apoiar causas ambientais de outras formas.",
-                afirmacao: "Apoio a causas ambientais, mas com métodos diferentes."
-            }
-        ]
-    },
-    {
-        enunciado: "Você está planejando seu futuro profissional. Como você considera o impacto ambiental na sua carreira?",
-        alternativas: [
-            {
-                texto: "Busco carreiras que possam contribuir para a preservação ambiental, como conservação, engenharia ambiental ou energias renováveis.",
-                afirmacao: "Prioridade no impacto ambiental positivo da carreira."
-            },
-            {
-                texto: "Escolho carreiras com base no potencial de ganhos e oportunidades de crescimento, independentemente do impacto ambiental.",
-                afirmacao: "Preferência por carreiras tradicionais sem foco ambiental específico."
-            }
-        ]
-    },
-];
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
-    if (atual < perguntas.length) {
-        perguntaAtual = perguntas[atual];
-        caixaPerguntas.textContent = perguntaAtual.enunciado;
-        mostraAlternativas();
-    } else {
-        // Fim das perguntas, exibir resultado final ou próxima ação
-        exibeResultadoFinal();
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
     }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
 function mostraAlternativas() {
-    // Limpa as alternativas anteriores
-    caixaAlternativas.innerHTML = '';
-
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
@@ -101,18 +33,25 @@ function mostraAlternativas() {
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    historiaFinal += `${opcaoSelecionada.afirmacao} `;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
-function exibeResultadoFinal() {
-    // Aqui você pode exibir o resultado final ou realizar a próxima ação
-    caixaPerguntas.textContent = "Quiz concluído! Veja seu resultado:";
-    caixaAlternativas.style.display = 'none'; // Esconder caixa de alternativas
-    textoResultado.textContent = `História final: ${historiaFinal}`;
-    caixaResultado.style.display = 'block'; // Exibir caixa de resultado
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+    caixaResultado.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
-// Iniciar o quiz mostrando a primeira pergunta
+function jogaNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
+
 mostraPergunta();
